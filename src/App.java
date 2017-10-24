c042da7663671d3661bd67677e3df3643c546ccf
cag2050 <cag2050@163.com>
2017-10-24 14:33:57 +0800

    m

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class App {
+    @Test
+    public void testAdd() {
+        String str = "junit is working fin";
+        assertEquals("junit is working fine", str);
+    }
+}
+
---
+++ src/TestApp.java
@@ -0,0 +1,15 @@
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class TestApp {
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(App.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println("failure info===");
+            System.out.println(failure.toString());
+        }
+        System.out.println("success info===");
+        System.out.println(result.wasSuccessful());
+    }
+}
